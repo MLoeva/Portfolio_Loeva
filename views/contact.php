98f977317b44c06fa23a63ec1c04577c5994c632
MLoeva <you@example.com>
2025-01-21 19:49:07 +0100

    ajout barre de nav à contacts

---
+++ views/contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <div id=entete>
+        <a href="/"><img src="images/logo.png" alt=""></a>
+        <ul class=navigation>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/about">A propos</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+</body>
+</html>
